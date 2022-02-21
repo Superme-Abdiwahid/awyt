f812b33427169e8ed85198c2e96c09b06edab8fb
Abdiwahid <ahajir@uw.edu>
2022-02-20 22:21:54 -0800

    For Ted Neward Help me!

--- app/src/main/java/edu/uw/abdiwahid/awyt/MainActivity.kt
+++ app/src/main/java/edu/uw/abdiwahid/awyt/MainActivity.kt
@@ -1,5 +1,7 @@
+
 package edu.uw.abdiwahid.awyt
 
+import android.annotation.TargetApi
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
@@ -9,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.telephony.SmsManager
 import android.util.Log
+
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -16,67 +19,107 @@ import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import java.lang.StringBuilder
+import android.os.Build
+import android.telephony.SmsMessage
+import androidx.annotation.BoolRes
+import androidx.annotation.DisplayContext
+import androidx.annotation.DrawableRes
+import androidx.annotation.MainThread
+import androidx.core.text.util.LinkifyCompat
+import androidx.core.view.WindowInsetsAnimationCompat
+import org.intellij.lang.annotations.Pattern
+import java.lang.annotation.Native
 
-class IntentListener: BroadcastReceiver(){
-    init{
+
+
+
+
+class IntentListener: BroadcastReceiver() {
+    init {
         Log.i("ABDIWAHID", "We created YYEEYEYE!!")
     }
-    override fun onReceive(p0: Context?, p1: Intent?) {
 
-    //var sms = Toast.makeText(this, "Recived ${p1}", Toast.LENGTH_LONG)
+    @TargetApi(Build.VERSION_CODES.M)
+    @MainThread
+    @SuppressWarnings("unknown")
+    @Suppress
+    @DisplayContext
 
-
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Toast.makeText(context, "Alarm Ringing...", Toast.LENGTH_SHORT).show()
     }
 }
-class MainActivity : AppCompatActivity() {
-    private var result: String = "";
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        var start: Button = findViewById<Button>(R.id.begin)
 
-        var stop: Button = findViewById<Button>(R.id.stop)
+    @MainThread
+    @DisplayContext
+    class MainActivity : AppCompatActivity() {
+        private var result: String = "";
 
-        var message: EditText = findViewById<EditText>(R.id.editTextTextPersonName)
+        @ExperimentalMultiplatform
+        @MainThread
+        @SuppressWarnings("unknown")
+        @Suppress
+        override fun onCreate(savedInstanceState: Bundle?) {
+            val alarmManager = this.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
+            val pendingIntent =
+                PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_NO_CREATE)
+            if (pendingIntent != null && alarmManager != null) {
+                alarmManager.cancel(pendingIntent)
+            }
 
 
-      stop.visibility = View.INVISIBLE
+            val broadCastIntent: Intent = Intent(this, IntentListener::class.java);
+            val intent = PendingIntent.getBroadcast(
+                this, 0, intent, 0
+            );
+            var alarmManager2: AlarmManager =
+                getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            alarmManager2.setRepeating(
+                AlarmManager.RTC_WAKEUP,
+                System.currentTimeMillis(),
+                AlarmManager.INTERVAL_HOUR, pendingIntent
+            );
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            var start: Button = findViewById<Button>(R.id.begin)
 
-        val alarmManager = getSystemService(Context.ALARM_SERVICE) as? AlarmManager
-        val time = System.currentTimeMillis()+ (5 * 1000)
-        val intent = Intent("HELLO")
-        val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
-        if (alarmManager != null) {
-            alarmManager.set(AlarmManager.RTC_WAKEUP, time, pendingIntent)
+            var stop: Button = findViewById<Button>(R.id.stop)
+
+            var message: EditText = findViewById<EditText>(R.id.editTextTextPersonName)
+
+
+            stop.visibility = View.INVISIBLE
+
+
+            val am = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            val recived = Intent(this, IntentListener::class.java)
+            var phone: EditText = findViewById<EditText>(R.id.editTextPhone)
+
+
+            phone.setOnClickListener() {
+                result = produceNumber(phone.text.toString())
+                Toast.makeText(this, "Your number is: ${result}", Toast.LENGTH_LONG)
+                Log.i("Abdiwahid", result)
+            }
+
+            Toast.makeText(this, "Your number is: ${result}", Toast.LENGTH_LONG)
+
         }
 
-        val am = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val recived = Intent(this, IntentListener::class.java)
-        var phone: EditText = findViewById<EditText>(R.id.editTextPhone)
-
-
-        phone.setOnClickListener(){
-            result = produceNumber(phone.text.toString())
-            Toast.makeText(this, "Your number is: ${result}", Toast.LENGTH_LONG )
-            Log.i("Abdiwahid", result)
-        }
-
-        Toast.makeText(this, "Your number is: ${result}", Toast.LENGTH_LONG )
-
-    }
-
-
-
-
-    fun produceNumber(phoneNumber: String): String {
-        var phone = ""
-       var  builder: StringBuilder = StringBuilder(10);
-        if(phoneNumber.length == 10) {
-            builder.append("(" + phoneNumber.substring(0,3) + ")" + "-");
-            builder.append(phoneNumber.substring(3,6) + "-" + phoneNumber.substring(6, 10))
-            return builder.toString()
-        }
+        @ExperimentalMultiplatform
+        @MainThread
+       @SuppressWarnings("unknown")
+       @Suppress
+        @Deprecated("unknown")
+        fun produceNumber(phoneNumber: String): String {
+            var phone = ""
+            var builder: StringBuilder = StringBuilder(10);
+            if (phoneNumber.length == 10) {
+                builder.append("(" + phoneNumber.substring(0, 3) + ")" + "-");
+                builder.append(phoneNumber.substring(3, 6) + "-" + phoneNumber.substring(6, 10))
+                return builder.toString()
+            }
 
             return "This is a  invalid number must be 10 digits";
+        }
     }
-}
